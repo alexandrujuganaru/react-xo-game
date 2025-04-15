560b2017e6cc139e87e63dd208d5fb4378c3cdb6
alexandrujuganaru <alexx.juganaru@gmail.com>
2025-04-15 20:07:26 +0300

    Added project pages + configured custom store

--- src/index.jsx
+++ src/index.jsx
@@ -5,22 +5,26 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import GameBoardPage from "./pages/game.board.page";
 import GameMenuPage from "./pages/game-menu.page";
 import RegisterPage from "./pages/register.page";
+import StoreProvider from "./store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* Tip: It provides specific hooks and data global accessible within child components */}
-    <BrowserRouter>
-      {/* Tip: The Routes must be parent of any set of routes */}
-      <Routes>
-        {/* Tip: The route helps us to create a page */}
-        <Route index={true} element={<RegisterPage />} />
-        {/* Nested routes helps use to group pages under specific prefixes */}
-        <Route path="game">
-          {/* Tip: We can acces the page under "/game/menu" */}
-          <Route path="menu" element={<GameMenuPage />} />
-          <Route path="board" element={<GameBoardPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    {/* Tip: It provides the store state which includes the data accesible within child components */}
+    <StoreProvider>
+      {/* Tip: It provides specific hooks and data global accessible within child components */}
+      <BrowserRouter>
+        {/* Tip: The Routes must be parent of any set of routes */}
+        <Routes>
+          {/* Tip: The route helps us to create a page */}
+          <Route index={true} element={<RegisterPage />} />
+          {/* Nested routes helps use to group pages under specific prefixes */}
+          <Route path="game">
+            {/* Tip: We can acces the page under "/game/menu" */}
+            <Route path="menu" element={<GameMenuPage />} />
+            <Route path="board" element={<GameBoardPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </StoreProvider>
   </StrictMode>
 );
